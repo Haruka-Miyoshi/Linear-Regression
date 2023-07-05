d22d70ea183ded6b050915820a05098fdee365c4
三好 遼[Liao] <89458986+Haruka-Miyoshi@users.noreply.github.com>
2023-07-05 21:43:04 +0900

    program 追加

---
+++ src/train.py
@@ -0,0 +1,27 @@
+# %%
+import os
+import torch
+import numpy as np
+from lr import LR
+import matplotlib.pyplot as plt
+
+if not os.path.exists('./figs'):
+    os.mkdir('./figs')
+
+x=torch.load('./data/x.txt')
+y=torch.load('./data/y.txt')
+
+model=LR(1,1)
+model.fit(x, y, mode=True)
+w1, b = model.get_params()
+
+x_h=np.array([-30, 30])
+y_h=w1*x_h+b
+plt.plot(x_h, y_h, "r")
+plt.title('fitting')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.scatter(x.numpy(), y.numpy())
+plt.savefig('./figs/fit.png')
+plt.show()
+# %%
